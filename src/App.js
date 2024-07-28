2de033c2ed746f6de100741c2f99c16604268861
OlyaMosienko <olya.ololoeva81@mail.ru>
2024-07-28 21:10:17 +0300

    Change form with React Hook Form & Yup

--- src/App.js
+++ src/App.js
@@ -1,46 +1,11 @@
-import * as yup from 'yup';
-// import { useForm } from 'react-hook-form';
-// import { yupResolver } from '@hookform/resolvers/yup';
-import { useRef, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useRef } from 'react';
+import { fieldsSchema } from './fieldsSchema';
 import styles from './App.module.css';
 
 const sendFormData = (formData) => {
 	console.log(formData);
-};
-
-const emailChangeSchema = yup
-	.string()
-	.required('Поле почты не должно быть пустым')
-	.matches(
-		/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/,
-		'Введите корректный почтовый адрес. Он должен содержать символ @. (Например, example@mail.ru)',
-	);
-
-const passwordChangeSchema = yup
-	.string()
-	.required('Поле пароля должно быть заполнено')
-	.matches(
-		/(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])/,
-		'Используйте более надежный пароль. Пароль должен содержать хотя бы одну цифру, хотя бы один символ и буквы латинского алфавита в верхнем и нижнем регистре',
-	)
-	.min(6, 'Пароль должен содержать не меньше 6 символов')
-	.max(20, 'Пароль не может содержать больше 20 символов');
-
-const passwordRepeatChangeSchema = yup
-	.string()
-	.required('Поле повтора пароля должно быть заполнено')
-	.oneOf([yup.ref('password')], 'Пароли не совпадают');
-
-const validateAndGetErrorMessage = (schema, value) => {
-	let errorMessage = null;
-
-	try {
-		schema.validateSync(value);
-	} catch ({ errors }) {
-		errorMessage = errors.join('\n');
-	}
-
-	return errorMessage;
 };
 
 export const App = () => {
@@ -49,94 +14,63 @@ export const App = () => {
 		password: '',
 		passwordRepeat: '',
 	};
-	const [formData, setFormData] = useState(initialFormState);
-	const [error, setError] = useState(null);
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isValid },
+	} = useForm({
+		mode: 'all',
+		defaultValues: initialFormState,
+		resolver: yupResolver(fieldsSchema),
+	});
+
 	const buttonSubmit = useRef(null);
 
-	const onSubmit = (event) => {
-		event.preventDefault();
-		if (checkAllFields()) {
-			sendFormData(formData);
-			setFormData(initialFormState);
-			setError(null);
-		}
-	};
-
-	const { email, password, passwordRepeat } = formData;
-
-	const isDisabledButton =
-		!!error || email === '' || password === '' || passwordRepeat === '';
-
-	const checkAllFields = () => {
-		const emailError = validateAndGetErrorMessage(emailChangeSchema, email);
-		const passwordError = validateAndGetErrorMessage(passwordChangeSchema, password);
-		const passwordRepeatError = validateAndGetErrorMessage(
-			passwordRepeatChangeSchema,
-			passwordRepeat,
-		);
-
-		if (emailError || passwordError || passwordRepeatError) {
-			setError(emailError || passwordError || passwordRepeatError);
-			return false;
-		}
-
-		setError(null);
-		return true;
-	};
-
-	const handleInputChange = (field, event) => {
-		const newValue = event.target.value;
-		setFormData({ ...formData, [field]: newValue });
-
-		if (field === 'passwordRepeat') {
-			const repeatError = validateAndGetErrorMessage(
-				passwordRepeatChangeSchema,
-				newValue,
-			);
-			setError(repeatError);
-		} else {
-			checkAllFields();
-			buttonSubmit.current.focus();
-		}
-	};
+	if (isValid) {
+		buttonSubmit.current.focus();
+	}
 
 	return (
 		<div className={styles.app}>
-			<form className={styles.form} onSubmit={onSubmit}>
+			<form className={styles.form} onSubmit={handleSubmit(sendFormData)}>
 				<input
 					className={styles.input}
 					type="email"
 					name="email"
-					value={email}
 					placeholder="Введите почту"
-					onChange={(event) => handleInputChange('email', event)}
+					{...register('email')}
 				/>
 				<input
 					className={styles.input}
 					type="password"
 					name="password"
-					value={password}
 					placeholder="Введите пароль"
-					onChange={(event) => handleInputChange('password', event)}
+					{...register('password')}
 				/>
 				<input
 					className={styles.input}
 					type="password"
 					name="passwordRepeat"
-					value={passwordRepeat}
 					placeholder="Повторите пароль"
-					onChange={(event) => handleInputChange('passwordRepeat', event)}
+					{...register('passwordRepeat')}
 				/>
 				<button
 					className={styles.btn}
-					ref={buttonSubmit}
 					type="submit"
-					disabled={isDisabledButton}
+					ref={buttonSubmit}
+					disabled={!isValid}
 				>
 					Зарегистрироваться
 				</button>
 			</form>
-			{error && <div className={styles.error}>{error}</div>}
+			{errors && (
+				<div className={styles.error}>
+					{errors.email?.message ||
+						errors.password?.message ||
+						errors.passwordRepeat?.message}
+				</div>
+			)}
 		</div>
 	);
 };
